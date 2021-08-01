ec9bf17d3f73c6ce8337a18c36e535eb144ec188
Ethan Hu <ethanh8093@gmail.com>
2021-08-01 18:34:00 +0000

    Basic Housekeeping

--- public/scripts/activities.js
+++ public/scripts/activities.js
@@ -4,3 +4,23 @@ function onClick() {
   clicks += 1;
   document.getElementById("clicks").innerHTML = clicks;
 };
+
+/*function countdown() {
+    var seconds = 60;
+    function tick() {
+        var counter = document.getElementById("counter");
+        seconds--;
+        counter.innerHTML = "0:" + (seconds < 10 ? "0" : "") + String(seconds);
+        if( seconds > 0 ) {
+            setTimeout(tick, 1000);
+        } else {
+            alert("Game over");
+        }
+    }
+    tick();
+}
+function timedOut() {
+    alert("Some error message");
+}
+setTimeout( timedOut , 60000 );
+countdown();*/
