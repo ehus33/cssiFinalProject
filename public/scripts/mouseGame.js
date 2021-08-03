c29a61916a1b8b61e3adc19a517c2d601be60860
Sebastian Ingino <sebasing@icloud.com>
2021-08-03 15:09:17 -0500

    Mouse Game fixes

--- public/scripts/mouseGame.js
+++ public/scripts/mouseGame.js
@@ -1,5 +1,9 @@
 var clicks = 0;
-function onClick() {
+document.getElementById('button').onclick = function() {
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
-};
+}
+
+document.getElementById("back").onclick = function() {
+    window.location = 'activities.html';
+}
